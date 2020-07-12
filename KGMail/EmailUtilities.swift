7e226aa6ba004efcbc525a04dd2ddf2ca5faa7af
Simon Bromberg <simon.bromberg@swiftmedical.io>
2020-07-12 12:52:13 -0400

    fix merge + tests

--- KGMail/EmailUtilities.swift
+++ KGMail/EmailUtilities.swift
@@ -1,43 +1,61 @@
 import Foundation
 
 class EmailUtilities {
-    static func combineAndSortEmails(_ emails: [[Email]]) -> [Email] {
-        return mergeSortedLists(firstList: [], restOfLists: emails)
-    }
-    
-    private static func mergeSortedLists(firstList: [Email], restOfLists: [[Email]]) -> [Email] {
-        guard let nextList = restOfLists.first else {
-            return firstList
-        }
-        
-        let secondList = mergeSortedLists(firstList: nextList, restOfLists: Array(restOfLists.dropFirst()))
-        
-        return mergeTwoSortedLists(firstList: firstList, secondList: secondList)
-    }
-    
-    private static func mergeTwoSortedLists(firstList: [Email], secondList: [Email]) -> [Email] {
-        // MERGE MESSAGES QUESTION
-        //
-        // This implementation is incomplete.
-        // Write logic to take the two arrays of Email objects and return a single array of all unique emails sorted from newest to oldest.
-        // Note that each list of Email objects is sorted.
-        // This function must run in O(n) time, where n is the sum of the sizes of the two lists
+	static func combineAndSortEmails(_ emails: [[Email]]) -> [Email] {
+		return mergeSortedLists(firstList: [], restOfLists: emails)
+	}
 
-		var result = [Email]()
-		let count = min(firstList.count, secondList.count)
-
-		for i in 0..<count {
-			let first = min(firstList[i], secondList[i])
-			let second = max(firstList[i], secondList[i])
-			result.append(first)
-			result.append(second)
+	private static func mergeSortedLists(firstList: [Email], restOfLists: [[Email]]) -> [Email] {
+		guard let nextList = restOfLists.first else {
+			return firstList
 		}
 
-		let rest = firstList.count > count ? firstList[count...] : secondList[count...]
-		result.append(contentsOf: Array(rest))
+		let secondList = mergeSortedLists(firstList: nextList, restOfLists: Array(restOfLists.dropFirst()))
 
-        return firstList + secondList
-    }
+		return mergeTwoSortedLists(firstList: firstList, secondList: secondList)
+	}
+
+	private static func mergeTwoSortedLists(firstList left: [Email], secondList right: [Email]) -> [Email] {
+		// MERGE MESSAGES QUESTION
+		// take the two arrays of Email objects and return a single array of all unique emails sorted from newest to oldest.
+		// Note that each list of Email objects is sorted.
+		// This function must run in O(n) time, where n is the sum of the sizes of the two lists
+
+		var result = [Email]()
+
+		var leftIndex = 0
+		var rightIndex = 0
+
+		while leftIndex < left.count && rightIndex < right.count {
+			let leftElement = left[leftIndex]
+			let rightElement = right[rightIndex]
+
+			if leftElement < rightElement {
+				result.append(leftElement)
+				leftIndex += 1
+			} else if leftElement > rightElement {
+				result.append(rightElement)
+				rightIndex += 1
+			} else {
+				result.append(leftElement)
+				leftIndex += 1
+				result.append(rightElement)
+				rightIndex += 1
+			}
+		}
+
+		while leftIndex < left.count {
+			result.append(left[leftIndex])
+			leftIndex += 1
+		}
+
+		while rightIndex < right.count {
+			result.append(right[rightIndex])
+			rightIndex += 1
+		}
+
+		return result
+	}
 }
 
 extension Email: Comparable {
--- KGMailTests/EmailUtilitiesTests.swift
+++ KGMailTests/EmailUtilitiesTests.swift
@@ -8,4 +8,38 @@ class EmailUtilitiesTests: XCTestCase {
     // This implementation is incomplete.
     // Write unit tests to test the functionality of EmailUtilities.combineAndSortEmails
 
+	func testEmpty() {
+		let result = EmailUtilities.combineAndSortEmails([])
+		XCTAssertTrue(result.isEmpty, "combine and sort emails did not handle empty array properly")
+	}
+
+	func testMerge() {
+		let email1 = Email(id: "id1", labelIds: [], snippet: "test", internalDate: "1594564151000", payload: EmailPayload(headers: []))
+		let email2 = Email(id: "id2", labelIds: [], snippet: "test2", internalDate: "1594564151002", payload: EmailPayload(headers: []))
+		let email3 = Email(id: "id3", labelIds: [], snippet: "test3", internalDate: "1594564151001", payload: EmailPayload(headers: []))
+
+		var emails = [[email1, email2], [email3]]
+		var merged = EmailUtilities.combineAndSortEmails(emails)
+
+		XCTAssertEqual(merged.count, 3, "Output does not have the correct number of emails")
+		XCTAssertEqual(merged[0].id, "id1")
+		XCTAssertEqual(merged[1].id, "id3")
+		XCTAssertEqual(merged[2].id, "id2")
+
+		emails = [[], [email1, email3, email2]]
+		merged = EmailUtilities.combineAndSortEmails(emails)
+		
+		XCTAssertEqual(merged.count, 3, "Output does not have the correct number of emails")
+		XCTAssertEqual(merged[0].id, "id1")
+		XCTAssertEqual(merged[1].id, "id3")
+		XCTAssertEqual(merged[2].id, "id2")
+
+		emails = [[email2], [email3], [email1]]
+		merged = EmailUtilities.combineAndSortEmails(emails)
+
+		XCTAssertEqual(merged.count, 3, "Output does not have the correct number of emails")
+		XCTAssertEqual(merged[0].id, "id1")
+		XCTAssertEqual(merged[1].id, "id3")
+		XCTAssertEqual(merged[2].id, "id2")
+	}
 }
